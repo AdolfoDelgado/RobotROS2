512da4296ce75f66e64209207e78223c9f1e8c13
AdolfoDelgado <adolfodelgado1233@gmail.com>
2026-08-20 01:02:34 -0600

    configuracion del slam toolbox incluido en el launch

--- src/mecanum_bridge/launch/slam.launch.py
+++ src/mecanum_bridge/launch/slam.launch.py
@@ -1,4 +1,4 @@
-import os
+""" import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -27,4 +27,67 @@ def generate_launch_description():
 
     return LaunchDescription([
         slam_toolbox_node
+    ]) """
+import os
+
+from launch import LaunchDescription
+from launch.actions import EmitEvent, RegisterEventHandler
+from launch.events import matches_action
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from ament_index_python.packages import get_package_share_directory
+from lifecycle_msgs.msg import Transition
+
+
+def generate_launch_description():
+
+    package_name = 'mecanum_bridge'
+
+    slam_config = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'slam.yaml'
+    )
+
+    slam_toolbox_node = LifecycleNode(
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        parameters=[
+            slam_config
+        ]
+    )
+
+    # Configurar SLAM Toolbox
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(slam_toolbox_node),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    # Activar SLAM Toolbox después de quedar INACTIVE
+    activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=slam_toolbox_node,
+            goal_state='inactive',
+            entities=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(
+                            slam_toolbox_node
+                        ),
+                        transition_id=Transition.TRANSITION_ACTIVATE,
+                    )
+                )
+            ],
+        )
+    )
+
+    return LaunchDescription([
+        slam_toolbox_node,
+        configure_event,
+        activate_event,
     ])
