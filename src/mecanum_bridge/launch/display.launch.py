8f05b4d3869a0de2e854620d99d9cc92e08e8e64
AdolfoDelgado <adolfodelgado1233@gmail.com>
2026-08-20 01:13:11 -0600

    configuracion de rviz

--- src/mecanum_bridge/launch/display.launch.py
+++ src/mecanum_bridge/launch/display.launch.py
@@ -35,12 +35,19 @@
 #         robot_state_publisher_node,
 #         rviz_node,
 #     ])
-
+from ament_index_python.packages import get_package_share_directory
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    
+    rviz_config = os.path.join(
+    get_package_share_directory('mecanum_bridge'),
+    'rviz',
+    'mecanum_slam.rviz'
+)
 
     rviz_node = Node(
         package='rviz2',
